61e0991f2342e2609bc8b1e26875f666188c51cf
duanvid <duw4ng@gmail.com>
2022-10-24 00:05:57 +0800

    update

--- src/handler.js
+++ src/handler.js
@@ -17,7 +17,7 @@ const addNoteHandler = (request, h) => {
 
   const isSuccess = notes.filter((note) => note.id === id).length > 0
 
-  if (isSuccess === true) {
+  if (isSuccess) {
     const response = h.response({
       status: 'success',
       message: 'Catatan berhasil ditambahkan',
@@ -26,6 +26,7 @@ const addNoteHandler = (request, h) => {
       }
     })
     response.code(201)
+    return response
   }
 
   const response = h.response({
@@ -34,7 +35,6 @@ const addNoteHandler = (request, h) => {
   })
 
   response.code(500)
-
   return response
 }
 
@@ -45,4 +45,82 @@ const getAllNotesHandler = () => ({
   }
 })
 
-module.exports = { addNoteHandler, getAllNotesHandler }
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params
+
+  const note = notes.filter((n) => n.id === id)[0]
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note
+      }
+    }
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'catatan tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+const editNoteByIdHandler = (request, h) => {
+  const { id } = request.params
+
+  const { title, tags, body } = request.payload
+  const updatedAt = new Date().toISOString()
+
+  const index = notes.findIndex((note) => note.id === id)
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt
+    }
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui'
+    })
+    response.code(200)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+const deleteNoteByHandler = (request, h) => {
+  const { id } = request.params
+
+  const index = notes.findIndex((note) => note.id === id)
+
+  if (index !== -1) {
+    notes.splice(index, 1)
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus'
+    })
+    response.code(200)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id Tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByHandler }
